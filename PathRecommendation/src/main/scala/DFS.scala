8727b8043cf7f580ee3ae8756990c205673cec79
ZechuanMiao <miao.ze@husky.neu.edu>
2019-04-20 12:46:47 -0400

    DFS & findpath

--- PathRecommendation/src/main/scala/DFS.scala
+++ PathRecommendation/src/main/scala/DFS.scala
@@ -1,31 +1,33 @@
+import java.util
+
 import org.apache.spark.graphx.VertexId
 
 import scala.collection.mutable.ArrayBuffer
+import java.util._
 
 object DFS {
-
-  def DFS(destination: Array[VertexId], start: Int, count: Int, temp: ArrayBuffer[VertexId], res:ArrayBuffer[ArrayBuffer[VertexId]]):Unit ={
-    if(count == 0){
-//      print(temp.mkString)
-      res.append(temp)
-      print(res)
-      temp.clear
-//      println(0)
-    } else {
-      for(i <- Range(start, destination.length)){
-        temp.append(destination(i))
-        DFS(destination, start+1, count-1, temp, res)
-        temp-=destination(i)
+  def DFS(destination: Array[VertexId]) ={
+    def recurse(destination: Array[VertexId], start: Int, count: Int, temp: ArrayList[VertexId], res:ArrayList[ArrayList[VertexId]]):Unit ={
+      if(count == 0){
+        print(temp)
+        res.add(new util.ArrayList[VertexId](temp))
+      }else{
+        for(i <- Range(0,destination.length)){
+          temp.add(destination(i))
+          val copy = destination.toBuffer
+          copy.remove(i)
+          recurse(copy.toArray,start+1,count-1,temp,res)
+          temp.remove(temp.size()-1)
+        }
       }
     }
+    val temp = new util.ArrayList[VertexId]()
+    val res = new util.ArrayList[ArrayList[VertexId]]()
+    recurse(destination,0,destination.length,temp,res)
+    res
   }
   def main(args: Array[String]): Unit ={
-
-    val destination:Array[VertexId] = Array(0,1,2,3)
-    val res = new ArrayBuffer[ArrayBuffer[VertexId]]()
-    DFS(destination, 0, 4, new ArrayBuffer[VertexId], res)
-//    println(res(0).mkString)
-
+//    println(DFS)
 
   }
 }
--- PathRecommendation/src/main/scala/PathRecommendation.scala
+++ PathRecommendation/src/main/scala/PathRecommendation.scala
@@ -1,27 +1,36 @@
+import java.awt.JobAttributes.DestinationType
+import java.util
+import java.util.ArrayList
+
 import org.apache.spark.graphx._
 import org.apache.spark.graphx.lib.ShortestPaths
 import org.apache.spark.graphx.lib.ShortestPaths.SPMap
+import org.apache.spark.sql.catalyst.expressions.ArrayBinaryLike
 import org.apache.spark.{SparkConf, SparkContext}
+
 import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 
-/***
+/** *
   * GraphUtils case class and it's related methods
+  *
   * @author Zechuan Miao & Lingyu Chen
   * @param graph
   */
-case class GraphUtils(graph: Graph[PartitionID, PartitionID]){
+case class GraphUtils(graph: Graph[PartitionID, PartitionID]) {
   /**
     * Get the components of given Graph
+    *
     * @param graph
     * @return VertexRDD of the components
     */
-  def getComponents(graph: Graph[VertexId, VertexId]) = {
-    graph.connectedComponents().vertices
+  def getStrongComponents(graph: Graph[PartitionID, PartitionID], numIter: Int) = {
+    graph.stronglyConnectedComponents(numIter)
   }
 
   /**
     * Given the graph and user provided nodes range, return the sub graph for operating
+    *
     * @param graph
     * @param scopeLeft
     * @param scopeRight
@@ -33,16 +42,17 @@ case class GraphUtils(graph: Graph[PartitionID, PartitionID]){
 
   /**
     * Filter out the desired shortest paths based on given nodes
-    * @param res result to store the
+    *
+    * @param res   result to store the
     * @param destination
     * @param paths the shortest paths from sub graph
     * @return Array buffer of (VertixId, SPMap)
     */
-  def filter(res: ArrayBuffer[(VertexId, SPMap)], destination: Array[VertexId], paths: Array[(VertexId, SPMap)])={
+  def filter(res: ArrayBuffer[(VertexId, SPMap)], destination: Array[VertexId], paths: Array[(VertexId, SPMap)]) = {
     for (i <- Range(0, destination.length - 1)) {
       val src: Array[(VertexId, SPMap)] = paths.filter({ case (vid, v) => vid == i })
-      for (j <-Range(i+1, destination.length - 1)) {
-        val path: Map[VertexId, PartitionID] = src(0)._2.filterKeys({case x => x==destination(j)})
+      for (j <- Range(i + 1, destination.length - 1)) {
+        val path: Map[VertexId, PartitionID] = src(0)._2.filterKeys({ case x => x == destination(j) })
         res += destination(i) -> path
       }
     }
@@ -51,8 +61,9 @@ case class GraphUtils(graph: Graph[PartitionID, PartitionID]){
 
   /**
     * Get the shortest paths, the format of the sortest path is (VertexId, SPMap)
+    *
     * @param graph
-    * @param src the start point
+    * @param src    the start point
     * @param target seq of destination points
     * @return Array of shortest paths
     */
@@ -66,7 +77,8 @@ case class GraphUtils(graph: Graph[PartitionID, PartitionID]){
 
   /**
     * Find the shortest path between two specific nodes
-    * @param src source node
+    *
+    * @param src    source node
     * @param target target node
     * @param graph
     * @return
@@ -77,20 +89,58 @@ case class GraphUtils(graph: Graph[PartitionID, PartitionID]){
       .filter({ case (vid, v) => vid == target })
   }
 
-  def findPath(src: VertexId, maps: Array[(VertexId, SPMap)],desNum: Int): Unit ={
+  def DFS(destination: Array[VertexId]) = {
+    def recurse(destination: Array[VertexId], start: Int, count: Int, temp: ArrayList[VertexId], res: ArrayList[ArrayList[VertexId]]): Unit = {
+      if (count == 0) {
+        print(temp)
+        res.add(new util.ArrayList[VertexId](temp))
+      } else {
+        for (i <- Range(0, destination.length)) {
+          temp.add(destination(i))
+          val copy = destination.toBuffer
+          copy.remove(i)
+          recurse(copy.toArray, start + 1, count - 1, temp, res)
+          temp.remove(temp.size() - 1)
+        }
+      }
+    }
 
+    val temp = new util.ArrayList[VertexId]()
+    val res = new util.ArrayList[ArrayList[VertexId]]()
+    recurse(destination, 0, destination.length, temp, res)
+    res
   }
 
-  def DFS(destination: Array[VertexId]): Unit ={
-
+  def findOnePath(src: VertexId, maps: Array[(VertexId, SPMap)], destination: Array[VertexId]) = {
+    var minPath: ArrayBuffer[(VertexId, SPMap)] = new ArrayBuffer[(VertexId, SPMap)]()
+    val max = 0
+    //the path from the startpoint
+    val fromSrc = maps.filter({ case (vid, v) => vid == src })(0)
+    //the path between all destinations
+    val dess: util.ArrayList[util.ArrayList[VertexId]] = DFS(destination)
+    for (p <- Range(0, dess.size())) {
+      val path: util.ArrayList[VertexId] = dess(p)
+      var sumlen = fromSrc._2.get(path(0)).get
+      val onepath =  new ArrayBuffer[(VertexId, SPMap)]()
+      for (i <- Range(0, destination.length - 1)) {
+        val pi: VertexId = path(i)
+        val pii: VertexId = path(i + 1)
+        val m: (VertexId, SPMap) = maps.filter({ case (vid, map) => vid == pi && map.get(pii) != 0})(0)
+        val len = m._2.filterKeys(_ == pii).get(pii)
+        sumlen += len.get
+        onepath += m
+      }
+      if(sumlen >= max){
+        minPath = onepath
+      }
+    }
   }
-
-
 }
 
 object PathRecommendation extends App {
   /**
     * Read in data of users
+    *
     * @return Tuple4 of required data: visiting area, location, destination list
     */
   def userData = {
@@ -129,8 +179,9 @@ object PathRecommendation extends App {
 
   /**
     * Read in the graph data, be careful that the reading process is directly, which means it follow the rule (source, target)
+    *
     * @param path the relative path of graph file
-    * @param sc target SparkContext to store the graph data
+    * @param sc   target SparkContext to store the graph data
     * @return
     */
   def readGraph(path: String, sc: SparkContext) = {
@@ -159,9 +210,8 @@ object PathRecommendation extends App {
     val gu = new GraphUtils(pa)
 
     val subgraph = gu.getSubgraph(pa, from, to)
-//    val fromStart: Array[(VertexId, SPMap)] = gu.getShortestPath(src, destination, subgraph).collect
+    //    val fromStart: Array[(VertexId, SPMap)] = gu.getShortestPath(src, destination, subgraph).collect
 
-    val res: Array[(VertexId, SPMap)] = gu.getShortestPath(subgraph,startpoint,destination)
-    println(res.mkString)
+    //    val res: Array[(VertexId, SPMap)] = gu.getShortestPath(subgraph,startpoint,destination)
   }
 }
